49fa207d69dd0a816fc2dd6debc08db4ec29bda1
Antsa Randriamihaja <arandriamihaja@gmail.com>
2017-04-03 15:45:43 -0400

    account setup

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
-var restify = require('restify');
-var builder = require('botbuilder');
-
+const restify = require('restify');
+const builder = require('botbuilder');
+const config = require('./config');
 
 //setup Restify server
 var server = restify.createServer();
-server.listen(process.env.port|| process.env.PORT || 3000, function(){
+server.listen(process.env.port|| process.env.PORT || 3978, function(){
     console.log('%s listening to %s', server.name, server.url);
 });
 
 //create chat bot
 
-var connecter = new builder.ChatConnector({
-    appId:
+const connector = new builder.ChatConnector({
+    appId:config.appId,
+    appPassword: config.appPassword
 });
+
+const bot = new builder.UniversalBot(connector);
